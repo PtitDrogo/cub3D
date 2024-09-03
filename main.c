85acf9c6f19765daa4b6158253635f41742985ec
Theo Freydiere <tfreydie@z2r2p4.42lyon.fr>
2024-09-03 16:26:30 +0200

    Instructions for parsing

---
+++ main.c
@@ -0,0 +1,9 @@
+int main(int argc, char const *argv[])
+{
+    //DO Parsing
+
+    //do the Cube
+
+    
+    return 0;
+}
--- newfiletfreydie.c
+++
@@ -1,5 +0,0 @@
-bonjour je suis tfreydie
-int main(int argc, char const *argv[])
-{    
-   return 0;
-}
