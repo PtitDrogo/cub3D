0b125900e80ec741e32ca020129029405384b496
tfreydie <ptitdrogo@DESKTOP-RDT98KN>
2024-09-28 22:31:08 +0200

    now loading the map the proper way, still need to add parsing to invalidate an empty line but at least it shows up on the map

--- sources/parsing/get_map.c
+++ sources/parsing/get_map.c
@@ -1,6 +1,6 @@
 #include "cub3D.h"
 
-int	count_lines(int cub_fd);
+static void replace_endline(char **map);
 
 char	**get_map_file2d(int cub_fd, int line_count)
 {
@@ -20,5 +20,26 @@ char	**get_map_file2d(int cub_fd, int line_count)
 			return (NULL);
 		i++;
 	}
+	replace_endline(map_file);
 	return (map_file);
+}
+
+static void replace_endline(char **map)
+{
+	int i;
+	int j;
+
+	j = 0;
+	while (map[j])
+	{
+		i = 0;
+		while (map[j][i])
+		{
+			if (map[j][i] == '\n')
+				map[j][i] = '\0';
+			i++;
+		}
+		j++;
+	}
+	return ;
 }
