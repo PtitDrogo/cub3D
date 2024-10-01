a456f1c56d378bc9529f23bee6e466334740a8a4
tfreydie <ptitdrogo@DESKTOP-RDT98KN>
2024-10-01 03:03:43 +0200

    better, scaling minimap

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -13,8 +13,8 @@
 
 # define MAX_HEIGHT 2100 // bruteforce, recuperable ailleurs
 # define MAX_LENGTH 3905 // taille max pour ecran mac
-# define DEFAULT_LENGTH 1200
-# define DEFAULT_HEIGHT 800
+# define DEFAULT_LENGTH 1700
+# define DEFAULT_HEIGHT 1200
 # define SPRITE_HEIGHT 64
 # define SPRITE_LENGTH 64
 # define CAM_SPEED 0.030
@@ -198,6 +198,6 @@ void	rotate_camera(t_info *w, int id);
 //minimap
 int 	display_minimap(t_info *w);
 void	move_player(t_info *w);
-int		generate_square(t_info *w, int x, int y, int color);
+int		generate_square(t_info *w, float x, float y, int color); //Changed this to float for the minimap but gotta double check for animation
 
 #endif
